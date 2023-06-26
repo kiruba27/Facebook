b1b380cea1905423bd453c476641672bdf636c68
kiruba27 <137761602+kiruba27@users.noreply.github.com>
2023-06-26 15:27:13 +0530

    Add files via upload
    login test case added

---
+++ Login_TC.java
@@ -0,0 +1,26 @@
+package Testcase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import SeleniumMaven.TestSelenium.Login_Pom;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login_TC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		
+		Login_Pom login = new Login_Pom(driver);
+		
+		login.enter_username("facebook");
+		login.enter_password("12345");
+		login.click_log();
+				
+	}
+
+}
